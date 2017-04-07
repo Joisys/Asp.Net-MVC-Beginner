a29bd65e68a9673793d09af9cb637a199ff91dd4
Joisys <getjose@gmail.com>
2017-04-07 17:07:03 +0100

    Project added with database connecton and grid.mvc

---
+++ Asp.Net-MVC-Beginner/Controllers/LocationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp.Net_MVC_Beginner.Data;
+
+namespace Asp.Net_MVC_Beginner.Controllers
+{
+    public class LocationController : Controller
+    {
+        // GET: Location
+        public ActionResult Index()
+        {
+            var context = new MvcBeginnerDataContext();
+            var locations = context.Locations.ToList();
+            return View(locations);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
